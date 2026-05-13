1f33dc93c46d0db8c2128532a2ffe135311bfdb6
md-sadidtahsin <sadidtahsin@hotmail.com>
2026-05-13 14:48:50 +0600

    ss

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,8 @@ set -e
 cd /var/www/react-app
 
 # Install dependencies and build React app
-npm install --legacy-peer-deps
-npm run build
+# npm install --legacy-peer-deps
+# npm run build
 
 # Copy build output to Nginx root
 rm -rf /usr/share/nginx/html/*
